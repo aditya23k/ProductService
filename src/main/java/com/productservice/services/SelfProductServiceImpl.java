0735f5ef5f7d68017ee425d8b15a0fc575018930
aditya23k <23adikam@gmail.com>
2023-10-08 22:04:21 +0530

    Added a new service class to show how annotations will be modified for preferential usage

---
+++ src/main/java/com/productservice/services/SelfProductServiceImpl.java
@@ -0,0 +1,12 @@
+package com.productservice.services;
+
+import com.productservice.models.Product;
+
+public class SelfProductServiceImpl implements ProductService {
+
+
+    @Override
+    public Product getProductbyId(Long id) {
+        return null;
+    }
+}
